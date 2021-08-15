5ab7fa4bc22f8f3d7f77f9b699f9aa830b8f492a
developer-suhag <developersuhag@gmail.com>
2021-08-15 12:36:18 +0600

    diposit and balance

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 // diposit
 document.getElementById('deposit-button').addEventListener('click', function () {
+    // set the diposit amount
+    const previousDipositTotal = document.getElementById('diposit-total');
+    const dipositPreviousText = previousDipositTotal.innerText;
+    const dipositPreviousAmount = parseFloat(dipositPreviousText)
+
     // get the diposit amount
     const dipositInput = document.getElementById('deposit-input')
-    const dipoistAmount = dipositInput.value;
-    // set the diposit amount
-    const dipositCurrent = document.getElementById('diposit-total');
-    dipositCurrent.innerText = dipoistAmount;
+    const dipositAmountText = dipositInput.value;
+    const newDipoistAmount = parseFloat(dipositAmountText);
+
+    // update diposit amount 
+
+    const totalDipositAmount = dipositPreviousAmount + newDipoistAmount;
+
+    previousDipositTotal.innerText = totalDipositAmount;
+
+    // update account balance 
+
+    const previousAccountBalance = document.getElementById('account-balance');
+    const previousBalanceText = previousAccountBalance.innerText;
+    const previousBalanceAmount = parseFloat(previousBalanceText);
+
+    // add deposit and balance 
+
+    const totalBalance = previousBalanceAmount + newDipoistAmount;
+
+    previousAccountBalance.innerText = totalBalance;
+
     // clear the diposit input after submit
     dipositInput.value = '';
 })
